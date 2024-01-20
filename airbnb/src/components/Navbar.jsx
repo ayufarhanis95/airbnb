06b21887ad64f63e991f8881ac7e515bc704efff
ayufarhanis95 <144410448+ayufarhanis95@users.noreply.github.com>
2024-01-21 03:51:34 +0800

    Added in a navbar and removed unnecessary files

---
+++ airbnb/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Navbar(){
+    return (
+        <div className="navbar">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/1200px-Airbnb_Logo_B%C3%A9lo.svg.png" alt="airbnblogo"></img>
+        </div>
+    )
+}
